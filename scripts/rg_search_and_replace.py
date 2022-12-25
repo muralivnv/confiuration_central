dac03a50cd2a6dd010e1be6cc0123e7bc9f50fa8
muralivnv <muralivnv@gmail.com>
2022-12-25 15:11:02 -0800

    add fzf based interactivity to search_and_replace

--- scripts/rg_search_and_replace.py
+++ scripts/rg_search_and_replace.py
@@ -5,6 +5,11 @@ import subprocess
 from argparse import ArgumentParser
 import sys
 
+# constants
+INTERACTIVE_CMD = '{RG_BASE_CMD} -l | fzf --sort --preview="{RG_BASE_CMD} --color=always -C 2 {}" --preview-window="up,70%:wrap" --ansi --bind="enter:execute({RG_BASE_CMD} --passthru {} > {}.temp && mv {}.temp {})" --cycle'
+NONINTERACTIVE_CMD = "{RG_BASE_CMD} -l | xargs -I @ sh -c '{RG_BASE_CMD} --passthru @ > @.temp && mv @.temp @' "
+
+# helper functions
 def create_rg_base_cmd(parsed_args) -> str:
     out = f"rg {parsed_args.old} {parsed_args.rg_opt}"
     if any(parsed_args.new):
@@ -12,18 +17,23 @@ def create_rg_base_cmd(parsed_args) -> str:
     return out
 
 def trigger(parsed_args) -> None:
-    rg_cmd_base = create_rg_base_cmd(parsed_args)
-    full_cmd = f"{rg_cmd_base}"
-    if not parsed_args.dry:
-        full_cmd = f"{rg_cmd_base} -l | xargs -I @ sh -c '{rg_cmd_base} --passthru @ > @.temp && mv @.temp @' "
-    subprocess.check_call(full_cmd, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+    rg_base_cmd = create_rg_base_cmd(parsed_args)
+    full_cmd = None
+    if parsed_args.interactive:
+        full_cmd = INTERACTIVE_CMD.replace("{RG_BASE_CMD}", rg_base_cmd)
+    else:
+        full_cmd = NONINTERACTIVE_CMD.replace("{RG_BASE_CMD}", rg_base_cmd)
+    try:
+        subprocess.check_call(full_cmd, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        pass
 
 if __name__ == "__main__":
     cli = ArgumentParser()
     cli.add_argument("-c", "--rg-opt", type=str, help="ripgrep options", dest="rg_opt", required=True)
     cli.add_argument("-o", "--old", type=str, help="pattern to replace", dest="old", required=True)
     cli.add_argument("-n", "--new", type=str, help="replace with", dest="new", required=False, default="")
-    cli.add_argument("-d", "--dry", help="do dry run", dest="dry", action="store_true")
+    cli.add_argument("-i", "--interactive", action="store_true", dest="interactive")
     parsed_args = cli.parse_args()
 
     trigger(parsed_args)
