c09cb40aa9bee59291edecc56ffa56a4e08f0412
muralivnv <muralivnv@gmail.com>
2023-06-21 21:43:02 -0700

    improve grep default args

--- .config/scripts/search_and_replace.py
+++ .config/scripts/search_and_replace.py
@@ -58,6 +58,7 @@ SED: https://www.gnu.org/software/sed/manual/sed.html
 
    If we wrap search items inside () then we can use \\number (0 to 9)
    sar -q "(\w*)\s*([:=])\s*(true|[0-9]{4})" --sed "'s/{QUERY}/\\U\\1 \E\\u\\2\E \\3/g'"
+
 Combining Multiple Files Using SED:
     sed '$ s/$//' FILE1 FILE2 FILE3 ... > OUTFILE
 """
@@ -70,10 +71,12 @@ FZF_ERR_CODE_TO_IGNORE = [0, 1, 130]
 #####
 def trigger(parsed_args) -> None:
     full_cmd = None
+
     SED_CMD = parsed_args.SED_CMD.replace("{QUERY}", parsed_args.QUERY)
     SED_CMD = SED_CMD.replace("{Q}", parsed_args.QUERY) # short-form
     SED_CMD = SED_CMD.replace("{REPLACE}", parsed_args.REPLACE)
     SED_CMD = SED_CMD.replace("{R}", parsed_args.REPLACE) # short-form
+
     if parsed_args.no_interactive:
         full_cmd = NONINTERACTIVE_CMD.replace("{GREP_FLAGS}", parsed_args.GREP_FLAGS)
         full_cmd = full_cmd.replace("{QUERY}", parsed_args.QUERY)
@@ -82,6 +85,7 @@ def trigger(parsed_args) -> None:
         full_cmd = INTERACTIVE_CMD.replace("{GREP_FLAGS}", parsed_args.GREP_FLAGS)
         full_cmd = full_cmd.replace("{QUERY}", parsed_args.QUERY)
         full_cmd = full_cmd.replace("{SED_CMD}", SED_CMD)
+
     try:
         subprocess.check_call(full_cmd, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
@@ -91,8 +95,11 @@ def trigger(parsed_args) -> None:
 if __name__ == "__main__":
     cli = ArgumentParser(formatter_class=RawTextHelpFormatter)
 
-    cli.add_argument("-g", "--grep", type=str, dest="GREP_FLAGS", default="-r -w", help="-r -w", required=False)
-    cli.add_argument("-s", "--sed", type=str, dest="SED_CMD", default="-i 's/{QUERY}/{REPLACE}/g'", help="-i 's/{QUERY}|{Q}/{REPLACE}|{R}/g'", required=False)
+    GREP_DEFAULTS = "-r -w --exclude-dir=\.* --exclude-dir=*build*"
+    SED_DEFAULT   = "-i 's/{QUERY}/{REPLACE}/g'"
+
+    cli.add_argument("-g", "--grep", type=str, dest="GREP_FLAGS", default=GREP_DEFAULTS, help=GREP_DEFAULTS, required=False)
+    cli.add_argument("-s", "--sed", type=str, dest="SED_CMD", default=SED_DEFAULT, help=SED_DEFAULT, required=False)
 
     cli.add_argument("-ni", action="store_true", dest="no_interactive", help="no interaction")
     cli.add_argument("--gist", "--man", action="store_true", dest="show_gist", help="show some gist", required=False)
