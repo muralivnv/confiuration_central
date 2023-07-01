ed6f4e8208a3fae5962c09a39c1bed9ab415c211
muralivnv <muralivnv@gmail.com>
2023-07-01 10:27:43 -0700

    finalize bookmark_server.py

--- .config/scripts/bookmark_server.py
+++ .config/scripts/bookmark_server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import http.server
 import socketserver
 import os
@@ -5,9 +7,9 @@ from mistletoe import Document, HTMLRenderer
 from argparse import ArgumentParser
 
 BOOKMARK_MD_FILE = ""
-PORT = 8000
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
     def do_GET(self):
         # Sending an '200 OK' response
         self.send_response(200)
@@ -19,7 +21,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
 
         contents = ""
-        with open(f"{BOOKMARK_MD_FILE}") as in_file:
+        with open(BOOKMARK_MD_FILE) as in_file:
             with HTMLRenderer() as renderer:
                 contents = renderer.render(Document(in_file))
 
@@ -29,19 +31,32 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
 if __name__ == '__main__':
     args = ArgumentParser()
-    args.add_argument("-f", "--file", dest="bookmark_file", help="bookmark file")
+    args.add_argument("-f", "--file", dest="bookmark_file", help="bookmark file",
+                      required=True)
+    args.add_argument("-p", "--port", dest="PORT", help="port",
+                      required=False, default=8000)
     
     parsed_args = args.parse_args()
     BOOKMARK_MD_FILE = parsed_args.bookmark_file
+    if not os.path.exists(BOOKMARK_MD_FILE):
+        print("[ERROR] specified file does not exist")
+        exit(1)
 
     # Create an object of the above class
-    handler_object = MyHttpRequestHandler
-    my_server = socketserver.TCPServer(("", PORT), handler_object)
+    bookmark_server = socketserver.TCPServer(("localhost", parsed_args.PORT),
+                                             MyHttpRequestHandler,
+                                             bind_and_activate=False)
+    bookmark_server.allow_reuse_address = True
+    bookmark_server.server_bind()
+    bookmark_server.server_activate()
 
     # Start the server
     try:
-        print(f"running server at -- http://0.0.0.0:{PORT}")
-        my_server.serve_forever()
+        print(f"running server at -- http://localhost:{parsed_args.PORT}")
+        bookmark_server.serve_forever()
     except KeyboardInterrupt:
         pass
-    my_server.server_close()
+    except Exception as e:
+        print(e)
+    finally:
+        bookmark_server.server_close()
