d389f8d358b23add5abee15428436ce61a088eb5
muralivnv <muralivnv@gmail.com>
2025-01-07 19:17:24 -0800

    add fonts and remove helix app

--- .config/bash_aliases.sh
+++ .config/bash_aliases.sh
@@ -1,8 +1,8 @@
 # setup custom aliases
 alias '..'='cd ..'
 alias 'rm_pkg_residual'="sudo apt remove --purge `dpkg -l | grep '^rc' | awk '{print $2}'`"
-alias hx='helix-23.05-x86_64.AppImage'
-alias helix='helix-23.05-x86_64.AppImage'
+alias hx='helix-25.01-x86_64.AppImage'
+alias helix='helix-25.01-x86_64.AppImage'
 alias sar='python3 ~/.config/scripts/search_and_replace.py'
 alias tr_trailing='sed -e "s/ \{1,\}$//"'
 alias inc="xargs -I {} echo '1 + {}' | bc"
